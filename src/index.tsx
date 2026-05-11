119a160c7700632451dfe34dd0f3031b89e3f3b0
Rob Petreski <robop72@gmail.com>
2026-05-12 08:32:03 +1000

    fix: remove missing index.css import

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
 import ChatInterface from './components/ChatInterface';
 
 const root = ReactDOM.createRoot(
