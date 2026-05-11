44ac793909a39bbbea8aaef354bccfa6ebedee54
Rob Petreski <robop72@gmail.com>
2026-05-11 23:08:27 +1000

    feat: add index entry point for react

---
+++ src/index.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import ChatInterface from './components/ChatInterface';
+
+const root = ReactDOM.createRoot(
+  document.getElementById('root') as HTMLElement
+);
+
+root.render(
+  <React.StrictMode>
+    <ChatInterface 
+      yearLevel="Year 9" 
+      subject="Mathematics" 
+      sessionId="pilot-test-01" 
+    />
+  </React.StrictMode>
+);
